ddc997ca04abdd3819cc0ccb8d1b743312223957
Roman <127163323+zodackwand@users.noreply.github.com>
2024-02-14 10:23:30 +0000

    Implemented add_friend func.

--- project1/main.py
+++ project1/main.py
@@ -5,12 +5,23 @@ class Node:
         self.location = location
         self.friends = []
 
-class Graph:
-    def __init__(self):
-        self.members = {}
+    def add_friend(self, friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
+            return True
+        else:
+            print(f"{self.name} is already friends with {friend.name}")
+            return False
 
     def find_friends(self, name):
         if name in self.members:
             return self.members[name].friends
         else:
             return []
+
+class Graph:
+    def __init__(self):
+        self.members = {}
+
+
+
