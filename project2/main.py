8a0eb9a1da237a57d86d925310c44065a6c84622
Roman <127163323+zodackwand@users.noreply.github.com>
2024-02-14 16:15:41 +0000

    Created classes for project 2

---
+++ project2/main.py
@@ -0,0 +1,10 @@
+# Each node is a computer
+class Node:
+    def __init__(self, name):
+        self.id = id
+
+# This class represents a network
+class Graph:
+    def __init__(self):
+        # all computers of the network as objects (nodes)
+        self.computers = []
