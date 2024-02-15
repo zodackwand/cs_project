5871751bc63df39fa9a4f3c1f9ee2d454a2853fb
RSLN-TECH <rustech2005@gmail.com>
2024-02-15 21:50:20 +0000

    Added ring & star connection methods

--- project2/main.py
+++ project2/main.py
@@ -8,14 +8,25 @@ class Graph:
         self.list = []
         # all computers with connections in a dict
         self.vertices = {}
+        
+    def __str__(self):
+        return f"{self.vertices}"
 
     def add_vertex(self, vertex):
         if vertex not in self.vertices:
+            self.list.append(vertex)
             # Create new dict elements with key: vertex and items: set of its connections
             self.vertices[vertex] = set()
             return True
 
     def remove_vertex(self, vertex):
+        # Delete vertex from ID list
+        self.list.remove(vertex)
+        # Delete all connections with vertex
+        for id in self.verices:
+            if vertex in self.vertices[id]:
+                self.vertices[id].remove(vertex)
+        
         return None
 
     def add_edge(self, vertex1, vertex2):
@@ -52,6 +63,63 @@ class Graph:
             formatted_matrix += ' '.join(str(x) for x in matrix[i]) + '\n'  # Add row elements
 
         return formatted_matrix
+    
+    def generate_ring_connection(self):
+        # Check if there is enough vertices to create a network
+        vertice_num = len(self.vertices)
+        if vertice_num < 3:
+            return None
+        
+        # Connect each ID vertice to its neighbor to form a ring
+        for id in range(vertice_num - 1):
+            vertex1 = self.list[id]
+            vertex2 = self.list[id + 1]
+            self.add_edge(vertex1, vertex2)
+            
+        # Connect the first and the last ID to complete the ring
+        self.add_edge(self.list[0], self.list[-1])
+        
+        return True
+    
+    def generate_star_connection(self, central_node = None):
+        # Check if the central node is in the network
+        if central_node not in self.list:
+            return None
+        
+        for id in self.list:
+            # Link every vertice to the central node
+            if id == central_node in self.vertices:
+                self.vertices[id] = set(self.list)
+            # Link specific vertice to the central node
+            else:
+                self.vertices[id] = {central_node}
+                
+        return True
+        
 
 def is_network_connected(network):
     return None
+
+# Sample raw network: A - B - C - D
+network = Graph()
+
+network.add_vertex('A')
+network.add_vertex('B')
+network.add_vertex('C')
+network.add_vertex('D')
+network.add_edge('A', 'B')
+network.add_edge('B', 'C')
+network.add_edge('C', 'D')
+
+# Modified by the ring method, each node has two neighbour connections and forms a ring
+network.generate_ring_connection()
+print(f"\nSample ring connection: {network}")
+
+# Modified by the star method, each node is linked to a central node specified by a user
+network.generate_star_connection('A')
+print(f"\nSample star connection: {network}")
+
+
+
+
+
